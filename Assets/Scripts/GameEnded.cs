12d354c55e52af5ef3ad6ae0c4c0966c84c0d8a9
DylanDr1 <dylan.dosramos@ontariotechu.net>
2024-10-20 13:30:49 -0400

    First two parts

---
+++ Assets/Scripts/GameEnded.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+
+public class GameEnded : MonoBehaviour
+{
+    public static bool GameEnd = false;
+
+    public GameObject GameEndedUI;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            End();
+        }
+    }
+
+    void End()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        GameEndedUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameEnd = true;
+    }
+
+    public void Retry()
+    {
+        GameEndedUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameEnd = false;
+        SceneManager.LoadScene(0);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
--- Assets/Scripts/Player/movement.cs
+++ Assets/Scripts/Player/movement.cs
@@ -8,7 +8,6 @@ public class movement : MonoBehaviour
 {
     public Camera playerCamera;
     public float walkSpeed = 6f;
-    public float runSpeed = 12f;
     public float jumpPower = 7f;
     public float gravity = 10f;
 
@@ -38,10 +37,8 @@ public class movement : MonoBehaviour
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         Vector3 right = transform.TransformDirection(Vector3.right);
 
-        // Press Left Shift to run
-        bool isRunning = Input.GetKey(KeyCode.LeftShift);
-        float curSpeedX = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Vertical") : 0;
-        float curSpeedY = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Horizontal") : 0;
+        float curSpeedX = canMove ? (walkSpeed) * Input.GetAxis("Vertical") : 0;
+        float curSpeedY = canMove ? (walkSpeed) * Input.GetAxis("Horizontal") : 0;
         float movementDirectionY = moveDirection.y;
         moveDirection = (forward * curSpeedX) + (right * curSpeedY);
 
@@ -85,4 +82,5 @@ public class movement : MonoBehaviour
             SceneManager.LoadScene(0);
         }
     }
+
 }
