9faff1be1923638b1d39915e8cf888d4102d5eb6
DylanDr1 <dylan.dosramos@ontariotechu.net>
2024-11-18 11:52:52 -0500

    Texture added

--- Assets/Scripts/OnOffTexture.cs
+++ Assets/Scripts/OnOffTexture.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.ParticleSystem;
 
 public class OnOffTexture : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject Wall1;
+    public GameObject Wall2;
+    public GameObject Wall3;
+    public GameObject Wall4;
+    public GameObject Floor1;
+    public GameObject Floor2;
 
+    public Material wall;
+    public Material floor;
+    public Material wall2;
+    public Material floor2;
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Alpha8))
+        {
+            Wall1.GetComponent<MeshRenderer>().material = wall;
+            Wall2.GetComponent<MeshRenderer>().material = wall;
+            Wall3.GetComponent<MeshRenderer>().material = wall;
+            Wall4.GetComponent<MeshRenderer>().material = wall;
+            Floor1.GetComponent<MeshRenderer>().material = floor;
+            Floor2.GetComponent<MeshRenderer>().material = floor;
+          
+        }
+        if (Input.GetKey(KeyCode.Alpha9))
+        {
+            Wall1.GetComponent<MeshRenderer>().material = wall2;
+            Wall2.GetComponent<MeshRenderer>().material = wall2;
+            Wall3.GetComponent<MeshRenderer>().material = wall2;
+            Wall4.GetComponent<MeshRenderer>().material = wall2;
+            Floor1.GetComponent<MeshRenderer>().material = floor2;
+            Floor2.GetComponent<MeshRenderer>().material = floor2;
+        }
+
     }
 }
