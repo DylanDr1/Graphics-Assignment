538fda67bf3e9d47395ce3d38ae6a29716bb9c67
DylanDr1 <dylan.dosramos@ontariotechu.net>
2024-11-26 14:03:21 -0500

    UpdatePole

--- Assets/Scripts/OnOffTexture.cs
+++ Assets/Scripts/OnOffTexture.cs
@@ -11,11 +11,16 @@ public class OnOffTexture : MonoBehaviour
     public GameObject Wall4;
     public GameObject Floor1;
     public GameObject Floor2;
+    public GameObject Pole1;
+    public GameObject Pole2;
+    public GameObject Pole3;
 
     public Material wall;
     public Material floor;
     public Material wall2;
     public Material floor2;
+    public Material pole;
+    public Material pole2;
     // Update is called once per frame
     void Update()
     {
@@ -27,7 +32,10 @@ public class OnOffTexture : MonoBehaviour
             Wall4.GetComponent<MeshRenderer>().material = wall;
             Floor1.GetComponent<MeshRenderer>().material = floor;
             Floor2.GetComponent<MeshRenderer>().material = floor;
-          
+            Pole1.GetComponent<MeshRenderer>().material = pole;
+            Pole2.GetComponent<MeshRenderer>().material = pole;
+            Pole3.GetComponent<MeshRenderer>().material = pole;
+
         }
         if (Input.GetKey(KeyCode.Alpha9))
         {
@@ -37,6 +45,9 @@ public class OnOffTexture : MonoBehaviour
             Wall4.GetComponent<MeshRenderer>().material = wall2;
             Floor1.GetComponent<MeshRenderer>().material = floor2;
             Floor2.GetComponent<MeshRenderer>().material = floor2;
+            Pole1.GetComponent<MeshRenderer>().material = pole2;
+            Pole2.GetComponent<MeshRenderer>().material = pole2;
+            Pole3.GetComponent<MeshRenderer>().material = pole2;
         }
 
     }
